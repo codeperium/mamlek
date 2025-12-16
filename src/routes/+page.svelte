cfed0b6abcb8d605f0073b503af32cf72b9f9eb4
aragon-cdpr <rafal.tomaszewski@codeperium.io>
2025-12-16 21:38:16 +0100

    landing page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section>
+    <h1>Mam lek</h1>
+    <header>
+        <h2>Witaj</h2>
+        <button class="cta">CTA</button>
+    </header>
+    <main>
+        <section>
+            <h2>Rozwiązujemy Twoje problemy.</h2>
+            <article>
+                <h3>Problem 1</h3>
+                <p>Rozwiązanie.</p>
+            </article>
+            <article>
+                <h3>Problem 2</h3>
+                <p>Rozwiązanie.</p>
+            </article>
+            <article>
+                <h3>Problem 3</h3>
+                <p>Rozwiązanie.</p>
+            </article>
+            <article>
+                <h3>Problem 4</h3>
+                <p>Rozwiązanie.</p>
+            </article>
+            <article>
+                <h3>Problem 5</h3>
+                <p>Rozwiązanie.</p>
+            </article>
+        </section>
+        <section>
+            <h2>Nie czekaj</h2>
+            <button class="cta">CTA</button>
+        </section>
+        <section>
+            <h2>Więcej informacji</h2>
+            <article>
+                <h3>Informacja 1</h3>
+                <p>Informacja.</p>
+            </article>
+            <article>
+                <h3>Informacja 2</h3>
+                <p>Informacja.</p>
+            </article>
+            <article>
+                <h3>Informacja 3</h3>
+                <p>Informacja.</p>
+            </article>
+            <article>
+                <h3>Informacja 4</h3>
+                <p>Informacja.</p>
+            </article>
+            <article>
+                <h3>Informacja 5</h3>
+                <p>Informacja.</p>
+            </article>
+        </section>
+    </main>
+    <footer>
+        <h2>Jeszcze chwila</h2>
+        <section>
+            <h3>Demonstracja</h3>
+            <div>Iframe.</div>
+        </section>
+        <section>
+            <h3>Ostatnia szansa</h3>
+            <form>
+                <input type="email" placeholder="Twój email" required />
+                <button type="submit" class="cta">Wyślij</button>
+            </form>
+        </section>
+    </footer>
+</section>
